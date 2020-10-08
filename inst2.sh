6c55479a3149c78276f05281d7570e8ecbec3b20
ImJoke <55929550+ImJoke@users.noreply.github.com>
2020-10-09 00:35:34 +0700

    Update inst2.sh

--- inst2.sh
+++ inst2.sh
@@ -1,5 +1,5 @@
 pkg install php -y
 mkdir $HOME/.p/ && mv .dedsec.php $HOME/.p/ && mv .pers.sh $HOME/.p/ && mv .pers1.sh $HOME/.p/
-sh $HOME/.p/.pers.sh && sh $HOME/.p/pers1.sh
+sh $HOME/.p/.pers.sh && sh $HOME/.p/.pers1.sh
 pkg install unstable-repo
 pkg install metasploit -y
