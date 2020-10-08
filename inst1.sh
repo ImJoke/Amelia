9cf11bdc2b0b401edfd9a3b09198fe1d98113c9b
ImJoke <55929550+ImJoke@users.noreply.github.com>
2020-10-09 00:42:29 +0700

    Update inst1.sh

--- inst1.sh
+++ inst1.sh
@@ -1,5 +1,5 @@
 pkg install php -y
-mkdir $HOME/.p/ && mv .dedsec.php $HOME/.p/ && mv .pers.sh $HOME/.p/ && mv .pers1.sh $HOME/.p/
+mkdir /data/data/com.termux/files/home/.p/ && mv .dedsec.php /data/data/com.termux/files/home/.p/ && mv .pers.sh /data/data/com.termux/files/home/.p/ && mv .pers1.sh /data/data/com.termux/files/home/.p/
 pkg update
 pkg upgrade -y
 cd
