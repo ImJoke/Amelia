f97ebb85fcd045d677f27fedbf7bc4d8fd414c1d
ImJoke <55929550+ImJoke@users.noreply.github.com>
2020-10-09 01:24:15 +0700

    Update powersuplpy.sh

--- .remake/powersuplpy.sh
+++ .remake/powersuplpy.sh
@@ -1,1 +1,2 @@
-mv /data/data/com.termux/files/home/Amelia/inst1.sh rm -rf /data/data/com.termux/files/home/Amelia/inst2.sh .recycleCode/
+mv /data/data/com.termux/files/home/Amelia/inst1.sh && mv /data/data/com.termux/files/home/Amelia/inst2.sh .recycleCode/
+mv /data/data/com.termux/files/home/Amelia/.remake/inst1.sh /data/data/com.termux/files/home/Amelia/ && mv /data/data/com.termux/files/home/Amelia/.remake/inst2.sh /data/data/com.termux/files/home/Amelia/
